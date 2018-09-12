3c877d51e89b46eca12bd4e3eed12df4a4411d4b
Denis Sidorenko <dongor7@gmail.com>
2018-09-12 21:05:46 +0300

    Init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
-module.exports = function getZerosCount(number, base) {
-  // your implementation
+function getZerosCount(number, base) {
+
+    let multipliers = factorize(base);
+    let maxMultiplier = 0;
+
+    for (let i = 0; i < multipliers.length; i++) {
+        if(maxMultiplier < multipliers[i])
+            maxMultiplier = multipliers[i];
+    }
+
+    let divider = maxMultiplier;
+    let zeros = 0;
+
+    while (number > divider) {
+        zeros += ~~(number / divider);
+        divider *= maxMultiplier;
+    }
+
+    return ~~zeros
+
 }
+
+function factorize(base) {
+    let j = 0;
+    let i = 2;
+    let a = [];
+    do {
+        if (base % i === 0) {
+            a[j] = i;
+            j++;
+            base = base / i;
+        } else{
+            i++;
+        }
+    }
+    while (i < base);
+    a[j] = i;
+
+    return a;
+}
+
+module.exports = getZerosCount;
