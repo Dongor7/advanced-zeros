34e5d9275aea3571b5d340009a13042bc4effa11
Dongor7 <dongor7@gmail.com>
2018-09-17 22:42:09 +0300

    100%

--- src/index.js
+++ src/index.js
@@ -1,42 +1,43 @@
-function getZerosCount(number, base) {
+module.exports = function getZerosCount(number, base){
 
-    let multipliers = factorize(base);
-    let maxMultiplier = 0;
+    let numBase = base;
+    let factorials = [];
 
-    for (let i = 0; i < multipliers.length; i++) {
-        if(maxMultiplier < multipliers[i])
-            maxMultiplier = multipliers[i];
-    }
-
-    let divider = maxMultiplier;
-    let zeros = 0;
-
-    while (number > divider) {
-        zeros += ~~(number / divider);
-        divider *= maxMultiplier;
-    }
-
-    return ~~zeros
-
-}
-
-function factorize(base) {
-    let j = 0;
-    let i = 2;
-    let a = [];
-    do {
-        if (base % i === 0) {
-            a[j] = i;
-            j++;
-            base = base / i;
-        } else{
-            i++;
+    for (let i = 2; i <= Math.sqrt(numBase); i++){
+        while (numBase % i === 0){
+            factorials.push(i);
+            numBase /= i;
         }
     }
-    while (i < base);
-    a[j] = i;
+    numBase !== 1 && factorials.push(numBase);
 
-    return a;
-}
+    let counter = [];
+    let newFactorials = [];
 
-module.exports = getZerosCount;
+    for (let i = 0, j = 0; i < factorials.length; i++, j++){
+        if (factorials[i] !== factorials[i - 1]){
+            newFactorials.push(factorials[i]);
+            counter.push(1);
+        }
+        else {
+            j--;
+            counter[j]++;
+        }
+    }
+
+    let zerosArray = [];
+
+    for (let i = 0; i < newFactorials.length; i++) {
+        numBase = number;
+        zerosArray.push(0);
+
+        while (numBase > 0) {
+            numBase = Math.floor(numBase / newFactorials[i]);
+            zerosArray[i] += numBase;
+        }
+
+        zerosArray[i] = Math.floor(zerosArray[i] / counter[i]);
+    }
+
+    return zerosArray.sort((a,b) => a - b)[0];
+};
